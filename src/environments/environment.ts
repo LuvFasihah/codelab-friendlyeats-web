946890979e3255ffc4521769c18dd3b52ab27620
LuvFasihah <fasihahasri98@gmail.com>
2025-01-07 11:24:27 +0800

    ang prob

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,10 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyB6Ra7BYtgEP6fsjY6D1QdzfXsQlFTT38A",
+    authDomain: "friendlychat-8b1ae.firebaseapp.com",
+    projectId: "friendlychat-8b1ae.firebaseapp.com",
+    storageBucket: "friendlychat-8b1ae.firebasestorage.app",
+    messagingSenderId: "472911258349",
+    appId: "1:472911258349:web:25d3adf8af88a0cd32a2e4",
   },
 };
